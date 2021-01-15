b911afbb0dbbd4980bf510a1987237b419e13a0c
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-15 18:05:17 -0300

    How to use html inside {{ }}

--- 2-Basics/app.js
+++ 2-Basics/app.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
 	data() {
 		return {
+			//courseGoalA: "<h1>Finish the course and learn Vue<h1>",
 			courseGoalA: "Finish the course and learn Vue",
 			courseGoalB: "Master Vue and build amazing apps",
 			vueLink: "https://vuejs.org/",
