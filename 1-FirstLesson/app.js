81fe0f3c42ac18287a3c0a836cd129809a3534b8
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-15 13:30:30 -0300

    End First Lesson

--- 1-FirstLesson/app.js
+++ 1-FirstLesson/app.js
@@ -1,3 +1,18 @@
+Vue.createApp({
+	data() {
+		return {
+			goals: [],
+			enteredValue: "",
+		};
+	},
+	methods: {
+		addGoal() {
+			this.goals.push(this.enteredValue);
+		},
+	},
+}).mount("#app");
+
+/*
 const buttonEl = document.querySelector("button");
 const inputEl = document.querySelector("input");
 const listEl = document.querySelector("ul");
@@ -11,3 +26,4 @@ function addGoal() {
 }
 
 buttonEl.addEventListener("click", addGoal);
+*/
