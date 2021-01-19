7363922f4f823a5dbbf9600accdc0327471a40f7
mlobf <marcoslemeborbafilho@gmail.com>
2021-01-18 22:18:05 -0300

    Reset input box

--- 26-DataBliding/app.js
+++ 26-DataBliding/app.js
@@ -1,22 +1,25 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: ''
-    };
-  },
-  methods: {
-    setName(event, lastName) {
-      this.name = event.target.value + ' ' + lastName;
-    },
-    add(num) {
-      this.counter = this.counter + num;
-    },
-    reduce(num) {
-      this.counter = this.counter - num;
-      // this.counter--;
-    }
-  }
+	data() {
+		return {
+			counter: 0,
+			name: "",
+		};
+	},
+	methods: {
+		setName(event, lastName) {
+			this.name = event.target.value;
+		},
+		add(num) {
+			this.counter = this.counter + num;
+		},
+		reduce(num) {
+			this.counter = this.counter - num;
+			// this.counter--;
+		},
+		resetInput() {
+			this.name = "";
+		},
+	},
 });
 
-app.mount('#events');
+app.mount("#events");
